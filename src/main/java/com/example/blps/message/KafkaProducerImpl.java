a50dd65521b768f7dc572eebd25dac4d9e671864
dec-a-dance <hakerlesha@gmail.com>
2022-05-21 00:35:41 +0300

    changed objects to strings in messaging.

--- src/main/java/com/example/blps/message/KafkaProducerImpl.java
+++ src/main/java/com/example/blps/message/KafkaProducerImpl.java
@@ -5,6 +5,8 @@ import com.example.blps.entities.OrderStatus;
 import com.example.blps.message.model.AbstractMessage;
 import com.example.blps.message.model.ChangeOrderStatusMessage;
 import com.example.blps.message.model.ProductsAppearedMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,24 +15,41 @@ import org.springframework.context.annotation.Bean;
 import java.util.Properties;
 
 public class KafkaProducerImpl {
-    private final KafkaProducer<Integer, AbstractMessage> producer;
+    private final KafkaProducer<Integer, String> producer;
+    private final ObjectMapper mapper;
 
     public KafkaProducerImpl(){
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("acks", "all");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.IntSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         this.producer = new KafkaProducer<>(props);
+        this.mapper = new ObjectMapper();
     }
 
-    public void sendProductsAppeared(Order o){
+    public boolean sendProductsAppeared(Order o){
         ProductsAppearedMessage mes = new ProductsAppearedMessage(o.getUser().getUsername(), o.getId(), o.getUser().getEmail());
-        producer.send(new ProducerRecord<>("products-appeared", mes.hashCode(), mes));
+        try {
+            String messageStr = mapper.writeValueAsString(mes);
+            producer.send(new ProducerRecord<>("products-appeared", mes.hashCode(), messageStr));
+            return true;
+        }
+        catch(JsonProcessingException e){
+            return false;
+        }
+
     }
 
-    public void sendChangeOrderStatus(Order o, OrderStatus newStatus){
+    public boolean sendChangeOrderStatus(Order o, OrderStatus newStatus) {
         ChangeOrderStatusMessage mes = new ChangeOrderStatusMessage(o.getUser().getUsername(), o.getId(), o.getUser().getEmail(), newStatus);
-        producer.send(new ProducerRecord<>("status-changed", mes.hashCode(), mes));
+        try {
+            String messageStr = mapper.writeValueAsString(mes);
+            producer.send(new ProducerRecord<>("status-changed", mes.hashCode(), messageStr));
+            return true;
+        }
+        catch(JsonProcessingException e){
+            return false;
+        }
     }
 }
